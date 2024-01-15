95e4454de25fe10f5e4d506e1a23ebaf08161ed5
Thomas Buss Precioso <thomas.precioso@hotmail.com>
2024-01-15 12:11:53 -0300

    att
    att

---
+++ Projetos Javascript/API-Rest/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'import/first': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-param-reassign': 'off',
+  },
+};
