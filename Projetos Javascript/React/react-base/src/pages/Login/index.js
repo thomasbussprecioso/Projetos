e5b928e751242be24c99ab8e93e21f79c9fa4f8e
Thomas Buss Precioso <thomas.precioso@hotmail.com>
2024-07-18 16:55:19 -0300

    att
    att

---
+++ Projetos Javascript/React/react-base/src/pages/Login/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>oie</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
+    </Container>
+  );
+}
---
+++ Projetos Javascript/React/react-base/src/services/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: '34.95.252.213',
+});
